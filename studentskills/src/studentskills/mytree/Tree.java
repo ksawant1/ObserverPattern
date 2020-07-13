1b50f3ccaa11638708ef3afd697a7c639fe187a8
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-07-13 06:57:49 +0530

    updated

--- studentskills/src/studentskills/mytree/Tree.java
+++ studentskills/src/studentskills/mytree/Tree.java
@@ -1,4 +1,60 @@
 package studentskills.mytree;
 
-public class Tree {
+public class Tree<K extends Comparable, V> {
+
+	/* Class containing left and right child of current node and key value*/
+		TreeNode<K, V> root;
+
+
+	/**
+	 * if node is null it creates a new node at that position
+	 * else recursively calls insertNode until it finds a null posiiton
+	 * @param key
+	 * @param value
+	 */
+	void insertNode(K key, V value) {
+		/* If the tree is empty, return a new node */
+
+		if (root == null)
+			root = new TreeNode(key, value);
+		else
+			root.insert(root, key, value);
+	}
+
+	/**
+	 * finds the key(bnumber) if present in tree
+	 * calls the getNode() method
+	 * @param key
+	 * @return node
+	 */
+	public V getValue(K key) {
+		TreeNode node = getNode(key);
+		return node == null ? null : (V) node.value;
+	}
+
+	private TreeNode getNode(K key) {
+		return root == null ? null : root.get(key);
+	}
+
+	// This method mainly calls inorder()
+	public void inorder() {
+		inorder(root);
+	}
+
+	/**
+	 * A utility function to do inorder traversal of Tree and print the node value
+	 * @param node
+	 */
+
+	private void inorder(TreeNode node) {
+		//System.out.println(root);
+		if (node != null) {
+			inorder(node.left);
+			System.out.println(node.value);
+			inorder(node.right);
+		}
+	}
+
 }
+
+
