c690514ebf8ed4c4765c809942d808a770da612f
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-07-13 06:40:36 +0530

    added case for modify file

--- studentskills/src/studentskills/mytree/Helper.java
+++ studentskills/src/studentskills/mytree/Helper.java
@@ -2,106 +2,79 @@ package studentskills.mytree;
 
 import studentskills.operation.Operation;
 
-import javax.print.DocFlavor;
 import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Helper {
-	TreeHelper tree = new TreeHelper();
-
-
-
+	TreeHelper treehelper = new TreeHelper();
 	/**
-	 * Helper is a utility that performs the operations for adding, removing videos calculating metrics and comsiders ad_requests
+	 * Helper is a utility that performs the operations for inserting, and modifying nodes into tree
 	 *
 	 * @author Krupa Sawant
 	 */
-
-
-
 		public Helper(){
-
 		}
-
 		static {
-
 		}
-		/**
-		 * printOutput matches an enum for required operation and processes the request
-		 * @exception VideoAlreadyExistsException if video exists, cannot add the same video
-		 * @exception VideoNotFoundException if video not found, cannot remove or ad requests for it
-		 * @exception SecurityException On not having necessary read permissions to the input file.
-		 * @exception FileNotFoundException On input file not found.
-		 * @exception IOException On any I/O errors while reading lines from input file.
-		 */
 
-		public void printOutput(String inputLine, String operationString) throws IOException {
+	/**
+	 *
+	 * @param inputLine
+	 * @param operationString
+	 * @throws IOException
+	 * @throws CloneNotSupportedException
+	 */
+
+		public void printOutput(String inputLine, String operationString) throws IOException, CloneNotSupportedException {
 
 			Operation operation = Operation.valueOf(operationString);
-
-
 			switch (operation) {
 				case INSERT:
-					insertInTree(inputLine);
+					insertInTree(inputLine,operation);
 					break;
+
 				case MODIFY:
-					//modifyTree(inputs[1]);
+					modifyTree(inputLine,operation);
 					break;
-				/*case AD_REQUEST:
-					String values[] = inputs[1].split("=");
-					try{
-						int length = Integer.parseInt(values[1]);
-						adRequest(length);
-						break;
-					}catch(NumberFormatException ex) {
-						throw new NumberFormatException("enter an integer value");
 
-
-					}
-				case METRICS:
-					String[] params = inputs[1].substring(1, inputs[1].length() -1 ).split(",");
-					updateMetrics(operationParams[1], params);
-					break;
-*/
 				default:
 					throw new IllegalArgumentException("invalid input format or file is empty");
 
 			}
 
 		}
-		/**
-		 * calculates metrics for every video based on given likes, views and dislikes
-		 */
 
-		public void insertInTree(String inputLine) throws IOException {
+	/**
+	 *
+	 * @param inputLine
+	 * @param operation
+	 * @throws IOException
+	 */
+
+		public void insertInTree(String inputLine,Operation operation) throws IOException {
 
 			StringBuilder result = new StringBuilder();
 			String newLine = System.getProperty("line.separator");
+
 			try {
 				Set<String> hash_Set
 						= new HashSet<String>();
 				String[] inputs = inputLine.split(":");
 				int bnumber= Integer.parseInt(inputs[0]);
-				System.out.println(bnumber);
 				String fname = (inputs[1].split(",")[0]);
-				System.out.println(fname);
 				String lname = (inputs[1].split(",")[1]);
-				System.out.println(lname);
 				String gpastr= (inputs[1].split(",")[2]);
 				double gpa= Double.parseDouble(gpastr);
-				System.out.println(gpa);
 				String major=(inputs[1].split(",")[3]);
 				for(int i=4; i<9;i++){
 					hash_Set.add(inputs[1].split(",")[i]);
 
 				}
-				System.out.println(hash_Set);
+
 				StudentRecord record= new StudentRecord(bnumber,fname,lname,gpa,major,hash_Set);
-				tree.insert(record);
-				tree.inorder();
+				treehelper.insert(record,operation);
+				//tree.inorder();
 				//System.out.println(prevState + "_POPULARITY_SCORE_UPDATE::" + context.getPopularity());
 				//result.append(prevState);
 				//result.append( "__POPULARITY_SCORE_UPDATE::" );
@@ -118,52 +91,18 @@ public class Helper {
 		 * decides whether the ad should be approved or rejected for every video based on given length
 		 */
 
-		/*public void adRequest(int length) throws IOException {
+		public void modifyTree(String inputLine,Operation operation) throws IOException, CloneNotSupportedException {
+			//System.out.println(inputLine);
 			StringBuilder result = new StringBuilder();
 			String newLine = System.getProperty("line.separator");
-			StateName prevState = context.getCurrentState();
-			String status = context.addRequest(length) ? "APPROVED" : "REJECTED";
-			//System.out.println(prevState + "_AD_REQUEST::" + status);
-			result.append(prevState);
-			result.append( "__AD_REQUEST::" );
-			result.append(status);
-			result.append(newLine);
-			resultw.store(result);
-		}*/
-		/**
-		 * removes given video by it's name
-		 */
+			String[] inputs = inputLine.split(":");
+			int replica_id= Integer.parseInt((inputs[0].split(",")[0]));
+			int bnumber = Integer.parseInt(inputs[0].split(",")[1]);
+	        String orig_value = (inputs[0].split(",")[2]);
+			String new_value= inputs[1];
+			treehelper.modify(replica_id,bnumber,orig_value,new_value,operation);
 
-		/*public void removeVideo(String videoName) throws IOException, VideoNotFoundException {
-
-			StringBuilder result = new StringBuilder();
-			String newLine = System.getProperty("line.separator");
-			StateName prevState = context.getCurrentState();
-			context = context.removeVideo(videoName);
-			//System.out.println(prevState + "_VIDEO_REMOVED::" + videoName);
-			result.append(prevState);
-			result.append( "__VIDEO_REMOVED::" );
-			result.append(videoName);
-			result.append(newLine);
-			resultw.store(result);
-		}*/
-		/**
-		 * adds given video by it's name
-		 */
-
-		/*public void addVideo(String videoName) throws IOException, VideoAlreadyExistsException {
-			Results results=new Results();
-			StringBuilder result = new StringBuilder();
-			String newLine = System.getProperty("line.separator");
-			StateName prevState = context.getCurrentState();
-			context = context.addVideo(videoName);
-			//System.out.println(prevState + "_ADD_VIDEO::" + videoName);
-			result.append(prevState);
-			result.append( "__VIDEO_ADDED::" );
-			result.append(videoName);
-			result.append(newLine);
-			resultw.store(result);
-		}*/
+		}
 
 		@Override
 		public String toString() {
